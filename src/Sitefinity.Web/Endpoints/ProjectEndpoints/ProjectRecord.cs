f466ce89999705233df9c8f41ecee02bf5ab9ba4
Ren John <rjohn@teamallegiance.com>
2023-10-16 17:08:59 -0400

    Removing unneccessary projects

--- src/Sitefinity.Web/Endpoints/ProjectEndpoints/ProjectRecord.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Sitefinity.Web.Endpoints.ProjectEndpoints;
-
-public record ProjectRecord(int Id, string Name);
--- src/Sitefinity.Web/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sitefinity.Web.ViewModels;
-
-public class ErrorViewModel
-{
-  public string? RequestId { get; set; }
-
-  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
